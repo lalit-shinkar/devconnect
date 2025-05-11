0741d60809bb3f4b611cdf27ebe62d5069c7b9a4
lalit <shinkarlalit18@gmail.com>
2025-05-11 15:35:56 +0000

    Fix: Add Flask app and bind to 0.0.0.0

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -9,4 +13,7 @@ def contact():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
 
